c041d7be34ecac16a102f07f07801a21d4483370
Doron <dreinsager@gmail.com>
2018-04-08 09:27:18 -0500

    added styles, updated from

--- register.php
+++ register.php
@@ -7,23 +7,33 @@
         <link rel="icon" href="favicon.ico">
         <title>Register</title>
     </head>
-    <div id="container">
     <body>
-        <form>
-            <label for="first" class="fname">First Name:</label>
-            <input type="text" id="first" name="first" placeholder="First Name"><br>
-            <label for="last">Last Name:</label>
-            <input type="text" id="last" name="last"  placeholder="Last Name"><br>
-            <label for="email">Email:</label>
-            <input type="text" id="email" name="email" placeholder="Email"><br>
-            <label for="password">Create Password:</label>
-            <input type="text" id="password" name="password" placeholder="Enter Password"><br>
-            <label for="confirm">Confirm Password:</label>
-            <input type="text" id="confirm" name="confirm" placeholder="Re-Enter Password"><br>
-            <div id="lower">
-            <input type="submit" value="Register"> 
-            </div>
+        <form method="post" action="register.php">
+        <?php include('s.php');?>
+        <h1>Registration Form</h1>
+         <div class="input-group">
+         <label for="username">Username</label>
+         <input type="text" name="username" placeholder="e.g. Molly666">
+         </div>
+         <div class="input-group">
+         <label for="email">Email</label>
+         <input type="text" name="email" placeholder="e.g. Molly666@gmail.com">
+         </div>
+         <div class="input-group">
+         <label for="password">Password</label>
+         <input type="text" name="password" placeholder="Password">
+         </div>
+         <div class="input-group">
+         <label for="confirm">Confirm Password</label>
+         <input type="text" name="confirm" placeholder="Confirm password">
+         </div>
+         <div class="input-group">
+         <button type="submit" name="register" class="btn">Register</button>
+         </div>
+         <p>Already a Member? 
+             <a href ="login.php"> Sign In </a>
+         </p>
+
         </form>
-    </div>
     </body>
 </html>
