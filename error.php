1b5ce319b4bedc44704137cc3d8c9316dae0f822
Doron <dreinsager@gmail.com>
2018-04-08 15:32:20 -0500

    trying php

--- error.php
+++ error.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php if (count($errors) > 0): ?>
 <div class="error">
 <?php foreach ($errors as $error): ?>
@@ -6,12 +5,3 @@
 <?php endforeach ?>
 </div>
 <?php endif ?> 
-=======
-<?php if (count($errors) > 0): ?>
-<div class="error">
-<?php foreach ($errors as $error): ?>
-<p><?php echo $error ?></p>
-<?php endforeach ?>
-</div>
-<?php endif ?> 
->>>>>>> 7acee3836a87ff97d59c1a154b4930f7b20f2f01
