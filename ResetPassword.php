0d396e3f7f05baaf8cba00a21b48cb07956639dc
Mark A. Garrett <magarret@go.olemiss.edu>
2018-04-08 10:20:57 -0500

    add divs for css reasons

--- ResetPassword.php
+++ ResetPassword.php
@@ -3,6 +3,7 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link href="https://fonts.googleapis.com/css?family=Jua" rel="stylesheet">
     <title>Password Rest</title>
     <link rel="stylesheet" href="styles.css">
     <link rel="icon" href="favicon.ico">
@@ -10,13 +11,20 @@
   <div id='container'>
   <body>
     <form>
-        <label>New Password: </label>
-        <input class='new-password' placeholder='Enter Password'><br>
-        <label>Confirm new password</label>
-        <input class='confirm-new-password' placeholder='Re-Enter Password'><br>
-        <div id='lower'>
-          <input type='submit' class='submit' value='submit'>
+        <?php include('s.php');?>
+        <div class='input-group'>
+          <label>New Password: </label>
+          <input class='new-password' placeholder='Enter Password'><br>
         </div>
+        <div class='input-group'>
+          <label>Confirm new password</label>
+          <input class='confirm-new-password' placeholder='Re-Enter Password'><br>
+        </div>
+        <div class='input-group'>
+          <div id='lower'>
+            <input type='submit' class='submit' value='submit'>
+          </div>
+        <div>
     </form>
   </body>
   </div>
