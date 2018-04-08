f819e260bc4102366ee0a3d1149422a22710d305
jguin95 <38171705+jguin95@users.noreply.github.com>
2018-04-08 10:49:29 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,22 +1,40 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="mystyle.css">
 </head>
 <body>
 
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="HP1.php">
+    <img src="logo.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+    Ole Miss Cybersecurity Challange
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="mainmenu.php">Secure area<span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+	
+    <span class="navbar-text">
+	<a href="login.php">Login</a>
+	<a href="register.php">Register</a>
+	<a href="logout.php">Logout</a>
+	
+    </span>
+  </div>
+</nav>
 
-<table id="hometable1" style="width:100%">
-	<tr>
-		<td class="ht1" width="33%" align="center">
-			<a href="login.php">Login</a>
-		</td>
-		<td class="ht1" width="33%" align="center">
-			<a href="logout.php">Logout</a>
-		</td>
-        <td class="ht1" width="33%" align="center">
-			<a href="register.php">Register</a>
-		</td>
-	</tr>
-</table>
+
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
