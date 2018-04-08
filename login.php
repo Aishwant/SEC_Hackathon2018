e2c498fcbad69397a090d86a8713597d20635b80
jguin95 <38171705+jguin95@users.noreply.github.com>
2018-04-08 10:26:47 -0500

    Add files via upload

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <head>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link href="https://fonts.googleapis.com/css?family=Jua" rel="stylesheet">
-        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="styles2.css">
         <link rel="icon" href="favicon.ico">
         <title>Login</title>
     </head>
